7cadf7aa18b847fe8b2bb71a321c0f99fbbce89e
quangduc <quangduc2520@example.com>
2021-10-26 22:43:46 +0700

    theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,15 @@ function scrollUp(){
     if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 300,
+    delay: 400,
+    // reset: true
+})
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+sr.reveal(`.category__data, .trick__content, .footer__content`,{interval: 100})
+sr.reveal(`.about__data, .discount__img`,{origin: 'left'})
+sr.reveal(`.about__img, .discount__data`,{origin: 'right'})
